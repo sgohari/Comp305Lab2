041a98c2fef85bb4aaa9233af66bbb88ed1f93d0
Nasir <31908515+sgohari@users.noreply.github.com>
2018-02-03 19:31:15 -0500

    lab2
    The lab2 was added

---
+++ Lab2/Assets/Script/button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+    public Sprite myCharactor;
+    public Rigidbody2D character;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        SpriteRenderer sRend = character.GetComponent<SpriteRenderer>();
+        sRend.sprite = myCharactor;
+    }
+}
